c46714ee2ade5501cfa5866ab8d9741092ffbdf4
farnood-m <mojahedzadeh.f@gmail.com>
2025-03-14 11:42:18 -0600

    comment deleted

--- src/Board.js
+++ src/Board.js
@@ -32,16 +32,16 @@ export default function Board() {
   }
   function calculateWinner(squares) {
     const lines = [
-      [0, 1, 2], // Horizontal rows
+      [0, 1, 2], 
       [3, 4, 5],
       [6, 7, 8],
-      [0, 3, 6], // Vertical columns
+      [0, 3, 6],
       [1, 4, 7],
       [2, 5, 8],
-      [0, 4, 8], // Diagonal lines
+      [0, 4, 8], 
       [2, 4, 6],
     ];
-    // Check each line for a winner
+
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (
@@ -49,10 +49,10 @@ export default function Board() {
         squares[a] === squares[b] &&
         squares[a] === squares[c]
       ) {
-        return squares[a]; // Return the winning symbol ('X' or 'O')
+        return squares[a];
       }
     }
-    return null; // No winner
+    return null
   }
 
   return (
